a7ca0a57110d3066abf2277caf6bed3a74801b62
full name <tanmayalpha@gmail.com>
2023-07-18 18:56:36 +0530

    Initial commit

---
+++ android/app/com/ziberto/MainActivity1.java
@@ -0,0 +1,7 @@
+package com.eatozfood_user;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity1 extends FlutterActivity {
+
+}
---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,26 @@
+final String appName = 'Eatoz Food';
+
+final String packageName = 'com.eatozfood_user';
+final String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+final String iosPackage = 'com.eatozfood_user';
+final String iosLink = 'your ios link here';
+final String appStoreId = '123456789';
+
+final String deepLinkUrlPrefix = 'https://eshopmultivendor.page.link';
+final String deepLinkName = 'Eatoz Food';
+
+final int timeOut = 50;
+const int perPage = 10;
+
+//final String baseUrl = 'https://vendor.eshopweb.store/app/v1/api/';
+// final String baseUrl = 'https://alphawizztest.tk/Eatoz Food_Ecom/app/v1/api/';
+// final String baseUrl = 'https://foodontheways.com/app/v1/api/';
+//final String baseUrl = 'https://foodontheways.com/New_food/app/v1/api/';
+final String baseUrl =
+    'https://developmentalphawizz.com/eatoz_food/app/v1/api/';
+final String imageUrl = 'https://developmentalphawizz.com/eatoz_food/';
+//final String imageUrl = 'https://foodontheways.com/';
+final String jwtKey = "0352e7a815f965e8e278d47976b8f9cf466e1f42";
+double latitudeFirst = 0;
+double longitudeFirst = 0;
